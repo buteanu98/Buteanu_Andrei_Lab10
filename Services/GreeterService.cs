cd25bf01cadf3888623e153b78b6b8ee3929cdd8
ANDREI-MIRCEA BUTEANU <buteanu.andrei154451@student.ubbcluj.ro>
2020-12-12 13:40:33 +0200

    Lab10

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -15,12 +15,53 @@ namespace Buteanu_Andrei_Lab10
             _logger = logger;
         }
 
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+
+        public override async Task SendStatusSS(SRequest request,IServerStreamWriter<SResponse> responseStream, ServerCallContext context)
         {
-            return Task.FromResult(new HelloReply
+            List<StatusInfo> statusList = StatusRepo();
+            SResponse sRes;
+            var i = 0;
+            while (!context.CancellationToken.IsCancellationRequested)
             {
-                Message = "Hello " + request.Name
-            });
+                sRes = new SResponse();
+                sRes.StatusInfo.Add(statusList.Skip(i).Take(request.No));
+                await responseStream.WriteAsync(sRes);
+                i++;
+
+                await Task.Delay(1000);
+            }
+        }
+        public override async Task<SResponse> SendStatusCS(IAsyncStreamReader<SRequest> requestStream, ServerCallContext context)
+        {
+            List<StatusInfo> statusList = StatusRepo();
+            SResponse sRes = new SResponse();
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                sRes.StatusInfo.Add(statusList.Skip(message.No - 1).Take(1));
+            }
+            return sRes;
+        }
+        public override async Task SendStatusBD(IAsyncStreamReader<SRequest> requestStream, IServerStreamWriter<SResponse> responseStream, ServerCallContext context)
+        {
+            List<StatusInfo> statusList = StatusRepo();
+            SResponse sRes;
+            await foreach (var message in requestStream.ReadAllAsync())
+            {
+                sRes = new SResponse();
+                sRes.StatusInfo.Add(statusList.Skip(message.No - 1).Take(1));
+                await responseStream.WriteAsync(sRes);
+            }
+        }
+        public List<StatusInfo> StatusRepo()
+        {
+            List<StatusInfo> statusList = new List<StatusInfo> {
+            new StatusInfo { Author = "Randy", Description = "Task 1 in progess"},
+            new StatusInfo { Author = "John", Description = "Task 1 just started"},
+            new StatusInfo { Author = "Miriam", Description = "Finished all tasks"},
+            new StatusInfo { Author = "Petra", Description = "Task 2 finished"},
+            new StatusInfo { Author = "Steve", Description = "Task 2 in progress"}
+            };
+            return statusList;
         }
     }
 }
